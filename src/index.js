06488949f28cf14569dd54350d6334db8352459a
Chirchir4 <gloriachirchir4@gmail.com>
2022-10-13 00:33:33 +0300

    instructions

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { MetricsProvider } from '@cabify/prom-react';
 import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
-import { ApolloProvider } from "@apollo/client";
-import { client } from './Api/Api';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ApolloProvider client={client}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ApolloProvider>
+
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+
 );
 
 
